f7338838a54da057060d45c1d9fb30f2d366b753
nmenny <nathanmenny@gmail.com>
2020-04-27 18:25:33 +0200

    Adding the main class to the project

---
+++ M2107-P2.1-PiesQuest/src/GameLauncher.java
@@ -0,0 +1,14 @@
+/**
+ * Launch a game of Pie's Quest
+ */
+public class GameLauncher {
+	
+	/**
+	 * The main of the game
+	 * @param args arguments of the main, not used
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
