67b53fee494ce30fa34a837047b6b1bac025f978
nmenny <nathanmenny@gmail.com>
2020-05-04 12:29:18 +0200

    Adding attributes to the Game

--- M2107-P2.1-PiesQuest/src/Game.java
+++ M2107-P2.1-PiesQuest/src/Game.java
@@ -3,4 +3,29 @@
  */
 public class Game {
 	
+	/**
+	 * The level which is being played
+	 */
+	private int currentLevel;
+	
+	/**
+	 * the player's character
+	 */
+	private Character character;
+	
+	/**
+	 * the parameter which will give some information about the window
+	 */
+	private Parameter parameter;
+	
+	/**
+	 * The levels of the game
+	 */
+	private Level[] levels;
+	
+	/**
+	 * The interface between the game and the player
+	 */
+	private IHM_Player ihm;
+	
 }
