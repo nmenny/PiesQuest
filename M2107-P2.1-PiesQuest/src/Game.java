ece28ff3144aa08cf4fffd6e3df5a94c9f5e2aef
nmenny <nathanmenny@gmail.com>
2020-05-04 13:29:21 +0200

    Adding methods to the Game class

--- M2107-P2.1-PiesQuest/src/Game.java
+++ M2107-P2.1-PiesQuest/src/Game.java
@@ -38,4 +38,93 @@ public class Game {
 		this.parameter = new Parameter(this);
 	}
 	
+	/**
+	 * Applies the modifications to the window
+	 * @param theNewVolume the new sound volume
+	 * @param theNewFormat the new screen format
+	 */
+	public void applyModification(int theNewVolume, String theNewFormat) {
+		
+	}
+	
+	/**
+	 * Displays all the levels at the screen
+	 */
+	public void displayAllLevels() {
+		
+	}
+	
+	/**
+	 * selects a level into the levels
+	 * @param levelId the index of the level
+	 */
+	public void chooseLevel(int levelId) {
+		
+	}
+	
+	/**
+	 * displays the selected level at the screen
+	 * @param levelId the index of the level
+	 */
+	public void displayLevel(int levelId) {
+		
+	}
+	
+	/**
+	 * makes the player move in a give direction
+	 * @param direction the given direction
+	 */
+	public void movePlayer(int direction) {
+		
+	}
+	
+	/**
+	 * Allows the player to jump
+	 */
+	public void jumpPlayer() {
+		
+	}
+	
+	/**
+	 * gives the index of the current level
+	 * @return the current level
+	 */
+	public int getCurrentLevel() {
+		return this.currentLevel;
+	}
+	
+	/**
+	 * displays the game over screen to the player
+	 */
+	public void displayGameOver() {
+		
+	}
+	
+	/**
+	 * Load a game save file
+	 */
+	public void loadElements() {
+		
+	}
+	
+	/**
+	 * displays the main screen to the player
+	 */
+	public void displayMainScreen() {
+		
+	}
+	
+	/**
+	 * displays the victory screen to the player
+	 */
+	public void displayVictoryScreen() {
+		
+	}
+	
+	/**
+	 * saves the game into a game save file
+	 */
+	public void save() {
+		
+	}
 }
