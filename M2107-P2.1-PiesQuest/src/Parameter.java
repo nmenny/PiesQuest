dacf11ce927b56d2bbb0bfd4a79cbca052282a26
nmenny <nathanmenny@gmail.com>
2020-05-04 12:10:24 +0200

    Adding attributes and constructor of Parameter

--- M2107-P2.1-PiesQuest/src/Parameter.java
+++ M2107-P2.1-PiesQuest/src/Parameter.java
@@ -2,5 +2,23 @@
  * Represents the parameters of the game to configure it
  */
 public class Parameter {
-
+	
+	/**
+	 * The sound volume of the game (in percent)
+	 */
+	private int volume;
+	
+	/**
+	 * The format in which the game will be displayed
+	 */
+	private String displayFormat;
+	
+	/**
+	 * Give standard parameters
+	 */
+	public Parameter() {
+		this.volume = 50;
+		this.displayFormat = "1920x1080";
+	}
+	
 }
