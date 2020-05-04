871a5d2111715e7d4bb6bdaa948b1bb2ccabd086
nmenny <nathanmenny@gmail.com>
2020-05-04 11:48:49 +0200

    Adding attributes to Character

--- M2107-P2.1-PiesQuest/src/Character.java
+++ M2107-P2.1-PiesQuest/src/Character.java
@@ -2,5 +2,15 @@
  * Represents a character on the game
  */
 public class Character {
-
+	
+	/**
+	 * The health of the Character
+	 */
+	private int health;
+	
+	/**
+	 * The name of the Character
+	 */
+	private String name;
+	
 }
