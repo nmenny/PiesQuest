a59113a1963b6d81a7522cba9777fc6e72120d63
nmenny <nathanmenny@gmail.com>
2020-05-04 11:47:00 +0200

    Adding attributes to Position

--- M2107-P2.1-PiesQuest/src/Position.java
+++ M2107-P2.1-PiesQuest/src/Position.java
@@ -2,5 +2,15 @@
  * Represents a position in a 2d environment
  */
 public class Position {
-
+	
+	/**
+	 * The coordinate in the X axis
+	 */
+	public int x;
+	
+	/**
+	 * The coordinate in the Y axis
+	 */
+	public int y;
+	
 }
