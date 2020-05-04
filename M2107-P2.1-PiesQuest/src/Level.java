15d4ae990378da646f76d1da6ce18c3e87ba6603
nmenny <nathanmenny@gmail.com>
2020-05-04 12:16:52 +0200

    Adding attributes for the level

--- M2107-P2.1-PiesQuest/src/Level.java
+++ M2107-P2.1-PiesQuest/src/Level.java
@@ -2,5 +2,24 @@
  * Represents a level of our game
  */
 public class Level {
-
+	
+	/**
+	 * The name of the level
+	 */
+	private final String name;
+	
+	
+	/**
+	 * The description of the level
+	 */
+	private final String description;
+	
+	/**
+	 * Is the level unlocked or not ?
+	 * <tt>true</tt> if we haven't finished the level yet
+	 * <tt>false</tt> else
+	 */
+	private boolean isLock;
+	
+	
 }
