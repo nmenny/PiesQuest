6ae593c4f88032db87146b2cebe30802fd2a9751
nmenny <nathanmenny@gmail.com>
2020-05-11 09:33:27 +0200

    Adding unimplemented method and the serial version of the IHM

--- M2107-P2.1-PiesQuest/src/IHM_Player.java
+++ M2107-P2.1-PiesQuest/src/IHM_Player.java
@@ -1,3 +1,4 @@
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
@@ -7,6 +8,11 @@ import javax.swing.JPanel;
  */
 public class IHM_Player extends JPanel implements Runnable, KeyListener {
 	
+	/**
+	 * Serial version
+	 */
+	private static final long serialVersionUID = 1L;
+
 	/**
 	 * The game being played
 	 */
@@ -31,6 +37,30 @@ public class IHM_Player extends JPanel implements Runnable, KeyListener {
 	 */
 	public void inform(String message) {
 		//TODO implement the method
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
