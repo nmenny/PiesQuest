6156f519f002716dd5b835e96d123df279a945b7
nmenny <nathanmenny@gmail.com>
2020-05-27 19:34:52 +0200

    Removing the previousDirection attribute

--- M2107-P2.1-PiesQuest/src/Game.java
+++ M2107-P2.1-PiesQuest/src/Game.java
@@ -49,11 +49,6 @@ public class Game {
 	private int currentSelection;
 	
 	/**
-	 * Stores the previous direction of the player movement
-	 */
-	private int previousDirection;
-	
-	/**
 	 * creates a new Game ready to be played
 	 * @param theIhm the interface between the game and the player
 	 */
@@ -65,7 +60,6 @@ public class Game {
 		//At the initialization, the main menu is displayed
 		this.menuDisplayed = 0;
 		this.currentSelection = 0;
-		this.previousDirection = 0;
 		try {
 			this.levels = Level.loadAllLevels();
 		} catch (LevelException e) {
@@ -128,9 +122,11 @@ public class Game {
 		//If the player will be at a position beyond the 2/3 of the screen width, the tiles moves
 		if((this.character.getPosition().x + Character.MOVING_SPEED > ((2 * this.parameter.getWidth()) / 3)) && (direction > 0)) {
 			this.levels[this.currentLevel].translation(1);
+			System.out.println(this.levels[this.currentLevel].getOffsetX());
 		//Else, if the player position is less than the 1/3 of the screen width and if the tiles are not back to normal (the starting offset) the tiles are moving the other way
 		} else if((this.character.getPosition().x - Character.MOVING_SPEED < (this.parameter.getWidth() / 3)) && (direction < 0) && (this.levels[this.currentLevel].getOffsetX() != 0)){
 			this.levels[this.currentLevel].translation(-1);
+			System.out.println(this.levels[this.currentLevel].getOffsetX());
 		} else  {  //The player moves
 			this.character.move(direction);
 		}
