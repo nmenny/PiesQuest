9a37d3be33f6b8c3aa2bfb8f6cb0cfe5a3236d2f
nmenny <nathanmenny@gmail.com>
2020-05-27 10:35:11 +0200

    Adding enumeration elements

--- M2107-P2.1-PiesQuest/src/EnumTiles.java
+++ M2107-P2.1-PiesQuest/src/EnumTiles.java
@@ -1,6 +1,21 @@
+import java.awt.Color;
+
 /**
  * Contains tiles character representation
  */
 public enum EnumTiles {
-
+	
+	Wall('x', Color.BLACK),
+	Player('p', Color.GREEN),
+	Strawberries('s', Color.RED),
+	End('e', Color.BLUE);
+	
+	public char charRepresentation;
+	
+	public Color tileColor;
+	
+	EnumTiles(char theRepresentation, Color theColor) {
+		this.charRepresentation = theRepresentation;
+		this.tileColor = theColor;
+	}
 }
