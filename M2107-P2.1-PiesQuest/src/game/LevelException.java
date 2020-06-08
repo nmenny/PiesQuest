7fab41d57ad1d8a88b3de7a1034de3660cd54ecd
nmenny <nathanmenny@gmail.com>
2020-06-08 13:22:19 +0200

    Adding a serial number

--- M2107-P2.1-PiesQuest/src/game/LevelException.java
+++ M2107-P2.1-PiesQuest/src/game/LevelException.java
@@ -3,4 +3,11 @@ package game;
 /**
  * An error when the level has a problem
  */
-public class LevelException extends Exception {}
+public class LevelException extends Exception {
+
+	/**
+	 * Serial ID of the Exception
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
