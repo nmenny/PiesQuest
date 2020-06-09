d1c93370527ae2d59f68492c07b9782eb11d980e
nmenny <nathanmenny@gmail.com>
2020-06-09 17:21:22 +0200

    Adding a test for the IHM_Player class

---
+++ test/test/IHM_PlayerTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import junit.framework.*;
+
+/**
+ * Tests the class IHM_Player
+ */
+public class IHM_PlayerTest extends TestCase {
+	
+	public IHM_PlayerTest(String testName) {
+		super(testName);
+	}
+	
+}
