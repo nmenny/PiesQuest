d34fe8c1923c949560eade5757219a9216bfbcd2
nmenny <nathanmenny@gmail.com>
2020-06-09 16:59:03 +0200

    Modifying test for the displayLevel method

--- test/GameTest.java
+++ test/GameTest.java
@@ -16,16 +16,16 @@ public class GameTest extends TestCase {
 		IHM_Player ihm = new IHM_Player(new JFrame());
 		Game game = ihm.getTheGame();
 		game.chooseLevel(-5);
-		assertEquals(game.getCurrentLevel(), 0); //It's equal to 0 since the value -5 is not applied
+		TestCase.assertEquals(game.getCurrentLevel(), 0); //It's equal to 0 since the value -5 is not applied
 		
 		game.chooseLevel(0);
-		assertEquals(game.getCurrentLevel(), 0);
+		TestCase.assertEquals(game.getCurrentLevel(), 0);
 		
 		game.chooseLevel(5);
-		assertEquals(game.getCurrentLevel(), 5); //Since there's at least 6 levels, it load the sixth level
+		TestCase.assertEquals(game.getCurrentLevel(), 5); //Since there's at least 6 levels, it load the sixth level
 		
 		game.chooseLevel(12);
-		this.assertFalse(game.getCurrentLevel() == 12); //There are 12 levels, so the thirteenth does not exist
+		TestCase.assertFalse(game.getCurrentLevel() == 12); //There are 12 levels, so the thirteenth does not exist
 		
 	}
 	
