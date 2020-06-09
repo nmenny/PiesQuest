cc4c718d5ef77b9e7621f809a799962d9d00920a
nmenny <nathanmenny@gmail.com>
2020-06-09 17:27:36 +0200

    Adding a new test for the method displayMenu()

--- test/test/IHM_PlayerTest.java
+++ test/test/IHM_PlayerTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import javax.swing.JFrame;
+
+import game.IHM_Player;
 import junit.framework.*;
 
 /**
@@ -9,6 +12,18 @@ public class IHM_PlayerTest extends TestCase {
 	
 	public IHM_PlayerTest(String testName) {
 		super(testName);
+	}
+	
+	/**
+	 * Tests the method displayMenu
+	 */
+	public void testDisplayMenu() {
+		IHM_Player ihm = new IHM_Player(new JFrame());
+		
+		ihm.displayMenu(0);
+		TestCase.assertEquals(ihm.getDisplayedMenu(), 0);
+		
+		
 	}
 	
 }
