cab614f2eb9bd583a112abe3c94233a300313cbc
nmenny <nathanmenny@gmail.com>
2020-06-14 15:42:08 +0200

    Adding attributes to the enumeration (LEFT and RIGHT)

--- M2107-P2.1-PiesQuest/src/game/EnumMovements.java
+++ M2107-P2.1-PiesQuest/src/game/EnumMovements.java
@@ -3,6 +3,27 @@ package game;
 /**
  * Contains the player's movement values
  */
-public class EnumMovements {
-
+public enum EnumMovements {
+	/**
+	 * Movement on the left
+	 */
+	LEFT(-1),
+	/**
+	 * Movement on the right
+	 */
+	RIGHT(1);
+	
+	/**
+	 * The value corresponding of the movement
+	 */
+	public int value;
+	
+	/**
+	 * Creates a new movement
+	 * @param theValue the value corresponding of the movement
+	 */
+	private EnumMovements(int theValue) {
+		this.value = theValue;
+	}
+	
 }
